dfc7fb5fe1f1f32498c7d02f5db26e72bf6326f0
Vishal Rathi <rajatrathi029@gmail.com>
2024-08-25 20:57:08 +0530

    y

--- src/Helper.js
+++ src/Helper.js
@@ -7,6 +7,7 @@ let getWeatherInfo = async (city) => {
         let resJson = await res.json();
         console.log(resJson);
         let result = {
+            id : resJson.weather[0].id,
             city: city,
             temp: resJson.main.temp,
             tempMin: resJson.main.temp_min,
@@ -21,4 +22,27 @@ let getWeatherInfo = async (city) => {
     }
 }
 
-export {getWeatherInfo};
+let weatherImage = (info)=>{
+    
+    let img ;
+
+    if(info.temp<15){
+        img = "https://images.unsplash.com/photo-1485594050903-8e8ee7b071a8?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    }
+    else if(info.humidity>80){
+        img = "https://images.unsplash.com/photo-1513774775025-b2612b7ec096?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    }
+    else if((info.id>750 && info.id<800) || info.id==721){
+        img ="https://images.unsplash.com/photo-1447014421976-7fec21d26d86?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    }
+    else if(info.id<750){
+        img = "https://images.unsplash.com/photo-1487621167305-5d248087c724?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    }
+    else{
+        img ="https://images.unsplash.com/photo-1601297183305-6df142704ea2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    }
+    
+    return img;
+}
+
+export {getWeatherInfo , weatherImage};
--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -10,11 +10,11 @@ import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 import LightModeIcon from '@mui/icons-material/LightMode';
 
 import './InfoBox.css'
+import {weatherImage} from './Helper'
 
 export default function InfoBox({ info }) {
-    const HOT_URL = "https://images.unsplash.com/photo-1500382017468-9049fed747ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c3VtbWVyJTIwZmFybXxlbnwwfHwwfHx8MA%3D%3D";
-    const COLD_URL = "https://images.unsplash.com/photo-1611492935712-8b93ad48080f?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-    const RAINY_URL = "https://images.unsplash.com/photo-1519692933481-e162a57d6721?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cmFpbiUyMHVtYnJlbGxhfGVufDB8fDB8fHww";
+    let img= weatherImage(info);
+    
     return (
         <div className="InfoBox">
             <div className='card-container'>
@@ -23,11 +23,11 @@ export default function InfoBox({ info }) {
                         component="img"
                         alt="green iguana"
                         height="140"
-                        image={info.humidity > 80 ? RAINY_URL : info.temp > 15 ? HOT_URL: COLD_URL}
+                        image={img || "https://images.unsplash.com/photo-1444858291040-58f756a3bdd6?q=80&w=1978&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
                     />
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div">
-                            {info.city}&nbsp;
+                        {info.city && `${info.city[0].toUpperCase()}${info.city.slice(1).toLowerCase()}`}&nbsp;
                             {info.humidity > 80 ? <ThunderstormIcon/> : info.temp > 15 ? <LightModeIcon/>: <AcUnitIcon/>}
                         </Typography>
                         <Typography variant="body2" color="text.secondary" component={"span"}>
